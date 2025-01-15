3a9bc9e8c9c0319dc9dfc03d52144703010d5d05
Luke Olson <olsonluke42@gmail.com>
2025-01-15 09:17:43 -0500

    lualine colors for testing

--- lua/lualine/themes/simple-green.lua
+++ lua/lualine/themes/simple-green.lua
@@ -15,32 +15,32 @@ local colors = {
 }
 return {
     normal = {
-        a = { bg = theme.visualselect, fg = theme.color, gui = 'bold' },
+        a = { bg = theme.visualselect, fg = theme.popup_menu, gui = 'bold' },
         b = { bg = colors.lightgray, fg = colors.white },
         c = { bg = colors.darkgray, fg = colors.gray }
     },
     insert = {
-        a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
+        a = { bg = theme.color_bg, fg = theme.visualselect, gui = 'bold' },
         b = { bg = colors.lightgray, fg = colors.white },
         c = { bg = colors.lightgray, fg = colors.white }
     },
     visual = {
-        a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+        a = { bg = theme.visualselect, fg = theme.color, gui = 'bold' },
         b = { bg = colors.lightgray, fg = colors.white },
         c = { bg = colors.inactivegray, fg = colors.black }
     },
     replace = {
-        a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+        a = { bg = theme.visualselect, fg = theme.color, gui = 'bold' },
         b = { bg = colors.lightgray, fg = colors.white },
         c = { bg = colors.black, fg = colors.white }
     },
     command = {
-        a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+        a = { bg = theme.color_bg, fg = theme.visualselect, gui = 'bold' },
         b = { bg = colors.lightgray, fg = colors.white },
         c = { bg = colors.inactivegray, fg = colors.black }
     },
     inactive = {
-        a = { bg = colors.darkgray, fg = colors.gray, gui = 'bold' },
+        a = { bg = theme.visualselect, fg = theme.popup_menu, gui = 'bold' },
         b = { bg = colors.darkgray, fg = colors.gray },
         c = { bg = colors.darkgray, fg = colors.gray }
     }
